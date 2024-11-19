a7feb0c1d7ad981a98dda4b5be7570cb83d1026a
Gabrieli da Luz Machado <112400420+gabrielidamachado@users.noreply.github.com>
2024-11-19 08:32:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+export const perguntas = [
+    {
+        enunciado: "O que são os problemas ambientais?",
+        alternativas: [
+            {
+                texto:"São o conjunto de desafios e problemas enfrentados pela Terra e seus sistemas naturais. Das mudanças climáticas e poluição á superpopulação e á utilização de energia, estas questões são complexas e interligadas",
+                afirmacao: ["São a poluição atmosférica, o aquecimento global, a poluição hídrica e dos solos, o desmatamento, as queimadas, as desertificação e a perda de biodiversidade.",
+                "resultam de uma combinação de causas naturais e impacto humano."
+                ]
+            },
+            {
+                texto: "Resultam de uma combinação de causas naturais e impacto humano.",
+                afirmacao: "afirmação"
+            }
+         ]
+    },
+    {
+        enunciado: "O que não representa o impacto ambiental?",
+        alternativas: [
+            {
+                texto: "A poluição sonora",
+                afirmacao: ["A infiltração da água.",
+                "chuva ácida.",
+                ]
+            },
+            {
+                texto: "Mobilidade hurbana.",
+                afirmacao: "chuva ácida",
+            }
+        ]
+    },
+    {
+        enunciado: "Quais problemas as mudanças climáticas trouxeram para o meio ambiente?",
+        alternativas: [
+            {
+                texto: "Aumento da temperatura,queimadas e derretimento das geleiras",
+                afirmacao: ["Um oceano cada vez mais quente e maior.",
+                    "As mudanças climáticas representam riscos para a sobrevivencia de espécies na terra e no oceano.",
+                     ] 
+            },
+            {
+                texto: "Afetam a  disponibilidade de água, tornando-a mais escassa em mais regiões.",
+                afirmacao: "afirmação"
+            }
+        ]
+    },
+    
+    
+            
+        ];
+
+
+      
